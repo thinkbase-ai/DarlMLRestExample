fdce3cfd23ae350299c96a977a33530a9b1c43e0
Andrew Edmonds <sales@docandys.com>
2018-06-12 17:48:10 +0100

    Initial version

---
+++ DarlMLRestExample/DarlMLData.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DarlMLRestExample
+{
+    /// <summary>
+    /// Used to perform a machine learning run
+    /// </summary>
+    public class DarlMLData
+    {
+        /// <summary>
+        /// Your DARL code
+        /// </summary>
+        /// <remarks>Should contain a single ruleset decorated with "supervised" containing only I/O. Outside of the ruleset the "pattern" parameter should be specified, along with mapinputs,mapoutputs and wires. MAP I/O should have paths.</remarks>
+        public string code { get; set; }
+
+        /// <summary>
+        /// The training data
+        /// </summary>
+        /// <remarks>this can be in XML or Json. If the former XPath should be used to specify paths in the ruleset. If the latter, JsonPath</remarks>
+        public string data { get; set; }
+
+        /// <summary>
+        /// Number of sets to use for numeric variables. Only values 3,5,7 and 9 can be specified.
+        /// </summary>
+        [Range(3,9)]
+        public int sets { get; set; }
+
+        /// <summary>
+        /// The percent to train on
+        /// </summary>
+        /// <remarks>Must be between 1 and 100</remarks>
+        [Range(1, 100)]
+        public int percentTrain { get; set; }
+
+        /// <summary>
+        /// email to send results
+        /// </summary>
+        /// <remarks>Because Machine learning can be CPU intensive training is performed via a queue in a secondary process. Results and the mined DARL will be emailed to this address.</remarks>
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+
+        /// <summary>
+        /// A name to identify the job in the returned email
+        /// </summary>
+        public string jobName { get; set; }
+
+    }
+}
--- DarlMLRestExample/Program.cs
+++ DarlMLRestExample/Program.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace DarlMLRestExample
 {
     class Program
     {
+
+        static string destEmail = "support@darl.ai"; //put your email address here!
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DarlML("yingyang").Wait();
+            DarlML("iris").Wait();
+            DarlML("cleveheart").Wait();
+        }
+
+        static async Task DarlML(string examplename)
+        {
+            var reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream($"DarlMLRestExample.{examplename}.darl"));
+            var source = reader.ReadToEnd();
+            reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream($"DarlMLRestExample.{examplename}.xml"));
+            var data = reader.ReadToEnd();
+            var spec = new DarlMLData { code = source, data = data, email = destEmail, percentTrain = 100, sets = 5, jobName = examplename};//use your own choice of training percent (1-100) and sets, (3,5,7,9)
+            var valueString = JsonConvert.SerializeObject(spec);
+            var client = new HttpClient();
+            var response = await client.PostAsync("https://darl.ai/api/Linter/DarlML", new StringContent(valueString, Encoding.UTF8, "application/json"));
+            //check for errors here...
         }
     }
 }
